2c05ac83c7e0d63b0c873569e74f6a367c353783
Vadym Hrashchenko <vhrashch@cisco.com>
2024-05-06 09:34:57 +0300

    sender grpc refactor

--- sender/internal/grpc/sender/handler.go
+++ sender/internal/grpc/sender/handler.go
@@ -9,6 +9,7 @@ import (
 	pb "github.com/hrvadl/goweekly/protos/gen/go/v1/sender"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/hrvadl/goweekly/sender/internal/platform/sender"
 )
@@ -17,6 +18,9 @@ func Register(gRPC *grpc.Server, s Sender, l *slog.Logger) {
 	pb.RegisterSenderServiceServer(gRPC, &server{
 		sender: s,
 		log:    l,
+		donech: make(chan struct{}),
+		errch:  make(chan error),
+		msgch:  make(chan *pb.SendRequest),
 	})
 }
 
@@ -28,57 +32,54 @@ type server struct {
 	pb.UnimplementedSenderServiceServer
 	sender Sender
 	log    *slog.Logger
+	donech chan struct{}
+	errch  chan error
+	msgch  chan *pb.SendRequest
 }
 
 func (srv *server) Send(s pb.SenderService_SendServer) error {
 	srv.log.Info("Got a send streaming request")
-	doneCh := make(chan struct{})
-	errCh := make(chan error)
 	ctx := s.Context()
 
-	go srv.handleStream(s, doneCh, errCh)
+	go srv.receive(s)
+	go srv.send(ctx)
 
 	for {
 		select {
 		case <-ctx.Done():
-			return fmt.Errorf("time out reached")
-		case err := <-errCh:
+			return ctx.Err()
+		case err := <-srv.errch:
 			return fmt.Errorf("failed to send msg: %w", err)
-		case <-doneCh:
+		case <-srv.donech:
+			s.SendAndClose(&emptypb.Empty{})
 			return nil
 		}
 	}
 }
 
-func (srv *server) handleStream(
-	s pb.SenderService_SendServer,
-	doneCh chan<- struct{},
-	errCh chan<- error,
-) {
-	breakCh := make(chan struct{})
-	ctx := s.Context()
-
-loop:
+func (srv *server) receive(s pb.SenderService_SendServer) {
 	for {
 		msg, err := s.Recv()
 		if errors.Is(err, io.EOF) {
-			doneCh <- struct{}{}
+			srv.donech <- struct{}{}
 			return
 		}
 
-		select {
-		case <-breakCh:
-			break loop
-		default:
-			go func() {
-				err = srv.sender.Send(ctx, sender.Message{
-					Message: msg.Message,
-				})
-				if err != nil {
-					breakCh <- struct{}{}
-					errCh <- err
-				}
-			}()
+		if err != nil {
+			srv.errch <- err
+			return
+		}
+
+		srv.msgch <- msg
+	}
+}
+
+func (srv *server) send(ctx context.Context) {
+	for msg := range srv.msgch {
+		srv.log.Debug("sending message...", "msg", msg.Message)
+		if err := srv.sender.Send(ctx, sender.Message{Message: msg.Message}); err != nil {
+			srv.errch <- fmt.Errorf("failed to send a message: %w", err)
+			return
 		}
 	}
 }
