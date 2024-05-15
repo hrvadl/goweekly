f56eaa9b57f5fba220c2efb6ece887f3f591e21d
Michael <michael.nilga@gmail.com>
2024-05-15 18:01:28 +0300

    Branch created

--- core/internal/processor/processor.go
+++ core/internal/processor/processor.go
@@ -1,57 +1,11 @@
 package processor
 
-import (
-	"fmt"
-	"time"
-
-	"golang.org/x/net/context"
-
-	"github.com/hrvadl/goweekly/core/internal/clients/rabbitmq/article"
-)
-
-//go:generate mockgen -destination=./mocks/mock_translator.go -package=mocks -source ./processor.go Translator
-type Translator interface {
-	Translate(ctx context.Context, msg string) (string, error)
-}
-
-//go:generate mockgen -destination=./mocks/mock_sender.go -package=mocks -source ./processor.go Sender
-type Sender interface {
-	Send(ctx context.Context, msg string) error
-}
-
-//go:generate mockgen -destination=./mocks/mock_formatter.go -package=mocks -source ./processor.go ArticleFormatter
-type ArticleFormatter interface {
-	FormatArticle(a article.Article) string
-}
-
-func New(fmter ArticleFormatter, sender Sender, translator Translator) *Processor {
-	return &Processor{
-		formatter:  fmter,
-		sender:     sender,
-		translator: translator,
-	}
-}
-
 type Processor struct {
-	sender     Sender
-	translator Translator
-	formatter  ArticleFormatter
+	//Implement logic
 }
 
-func (p *Processor) Process(a article.Article) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	translated, err := p.translator.Translate(ctx, a.Content)
-	if err != nil {
-		return fmt.Errorf("failed to translate article: %w", err)
-	}
-
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second*3)
-	defer cancel()
-	a.Content = translated
-	if err := p.sender.Send(ctx, p.formatter.FormatArticle(a)); err != nil {
-		return fmt.Errorf("failed to translate article: %w", err)
-	}
+func (p *Processor) Process() error {
+	//Implement logic
 
 	return nil
 }
